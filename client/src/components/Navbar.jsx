650f5c0ecbc29a748010b3e0dc767a96c4516332
PriyanshuQ <80645068+PriyanshuQ@users.noreply.github.com>
2023-05-04 14:36:33 +0530

    commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -76,15 +76,7 @@ const Navbar = () => {
               ))}
             </ul>
             <div className='flex mx-4'>
-              <CustomButton 
-                btnType="button"
-                title={address ? 'Create a campaign' : 'Connect'}
-                styles={address ? 'bg-[#1dc071]':'bg-[#8c6dfd]'}
-                handleClick={()=> {
-                if(address) navigate('create-campaign')
-                else 'connect()'
-                }}
-              />
+              
             </div>
           </div>
       </div>
