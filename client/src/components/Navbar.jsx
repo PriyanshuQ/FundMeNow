46f686a7377c234ff6d77a7a367b3953b1bf149a
PriyanshuQ <80645068+PriyanshuQ@users.noreply.github.com>
2023-05-07 14:35:48 +0530

    commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -26,7 +26,10 @@ const Navbar = () => {
           btnType="button"
           title={address ? 'Create a campaign' : 'Connect'}
           styles={address ? 'bg-[#1dc071]':'bg-[#8c6dfd]'}
-          
+          handleClick={()=> {
+            if(address) navigate('create-campaign')
+            else 'connect()'
+          }}
         />
 
         <Link to="/profile">
