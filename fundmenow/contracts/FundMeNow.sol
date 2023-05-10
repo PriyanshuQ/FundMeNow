0462f0060fa31a8be876452567f303d45bd6aace
PriyanshuQ <80645068+PriyanshuQ@users.noreply.github.com>
2023-05-10 15:01:46 +0530

    commit

--- fundmenow/contracts/FundMeNow.sol
+++ fundmenow/contracts/FundMeNow.sol
@@ -38,10 +38,34 @@ contract FundMeNow {
         return numberOfCampaigns - 1;
     }
 
-    function donateToCampaign() {}
-    
-    function getDonators() {}
-    
-    function getCampaigns() {}
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
 
+        Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
+
+        if(sent){
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
+    
+    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
+    
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+
+        for(uint i=0;i<numberOfCampaigns;i++){
+            Campaign storage item = campaigns[i];
+
+            allCampaigns[i] = item;
+        }
+
+        return allCampaigns;
+    }
 }
