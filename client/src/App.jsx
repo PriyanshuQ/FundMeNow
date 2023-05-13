bc312e026671e687348855fb0a0a0df88d093de2
PriyanshuQ <80645068+PriyanshuQ@users.noreply.github.com>
2023-05-13 16:23:39 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
+import { Sidebar, Navbar } from './components'
+import { CampaignDetails, CreateCampaign, Home, Profile } from './pages';
+
 const App = () => {
   return (
     <div className='relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row'>
       <div className='sm:flex hidden mr-10 relative'>
-        Sidebar
+        <Sidebar />
       </div>
 
       <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
-        Navbar
+        <Navbar />
 
         <Routes>
           <Route path="/" element={<Home />} />
