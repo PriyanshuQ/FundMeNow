5ab2adc83693ac87a887b8f17ace07dca8a7e4d4
PriyanshuQ <80645068+PriyanshuQ@users.noreply.github.com>
2023-05-15 20:17:45 +0530

    commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, {useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+
+import { CustomButton } from './';
+import { logo, menu, search, thirdweb } from '../assets';
+import { navlinks } from '../constants';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const [isActive, setIsActive] = useState('dashboard');
+  const [toggleDrawer, setToggleDrawer] = useState(false)
   return (
-    <div>Navbar</div>
+    <div className='flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6'>
+      <div className='lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24]'>
+        <input type="text" placeholder='Search for campaigns' className='flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none' />
+        <div className='w-[72px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center items-center cursor-pointer'>
+          <img src={search} alt="search" className='w-[15px] h-[15px] object-contain' />
+        </div>
+      </div>
+    </div>
   )
 }
 
