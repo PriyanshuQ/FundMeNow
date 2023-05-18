e3741ac1c35874a754863ed3289362fefea95fee
PriyanshuQ <80645068+PriyanshuQ@users.noreply.github.com>
2023-05-19 00:47:47 +0530

    commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -40,27 +40,54 @@ const Navbar = () => {
       </div>
 
       {/* Small screen navigation */}
-          <div className='sm:hidden flex justfy-between items-center relative'>
-            <div className='w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer'>
-              <img src={thirdweb} alt="user" className='w-[60%] h-[60%] object-contain' />
-            </div>
+      <div className="sm:hidden flex justify-between items-center relative">
+        <div className="w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer">
+            <img src={logo} alt="user" className="w-[60%] h-[60%] object-contain" />
+          </div>
 
           <img 
-            src={menu} 
-            alt="menu" 
-              className='w-[34px] h-[34px] object-contain cursor-pointer'
-              onClick={() => setToggleDrawer(!toggleDrawer)}
+            src={menu}
+            alt="menu"
+            className="w-[34px] h-[34px] object-contain cursor-pointer"
+            onClick={() => setToggleDrawer((prev) => !prev)}
           />
-          <div className={`absolute top-[50px] right-0 left-0 bg-[#1c1c24] z-10 shadow-secondary py-4 ${!toggleDrawer} ? '-translate-y-[100vh]' : 'translate-y-0]' transition-all duration-700`}></div>
-            <ul className='mb-4'>
+
+          <div className={`absolute top-[60px] right-0 left-0 bg-[#1c1c24] z-10 shadow-secondary py-4 ${!toggleDrawer ? '-translate-y-[100vh]' : 'translate-y-0'} transition-all duration-700`}>
+            <ul className="mb-4">
               {navlinks.map((link) => (
-                <li>
-                  
+                <li
+                  key={link.name}
+                  className={`flex p-4 ${isActive === link.name && 'bg-[#3a3a43]'}`}
+                  onClick={() => {
+                    setIsActive(link.name);
+                    setToggleDrawer(false);
+                    navigate(link.link);
+                  }}
+                >
+                  <img 
+                    src={link.imgUrl}
+                    alt={link.name}
+                    className={`w-[24px] h-[24px] object-contain ${isActive === link.name ? 'grayscale-0' : 'grayscale'}`}
+                  />
+                  <p className={`ml-[20px] font-epilogue font-semibold text-[14px] ${isActive === link.name ? 'text-[#1dc071]' : 'text-[#808191]'}`}>
+                    {link.name}
+                  </p>
                 </li>
               ))}
             </ul>
+            <div className='flex mx-4'>
+              <CustomButton 
+                btnType="button"
+                title={address ? 'Create a campaign' : 'Connect'}
+                styles={address ? 'bg-[#1dc071]':'bg-[#8c6dfd]'}
+                handleOnClick={()=> {
+                if(address) navigate('create-campaign')
+                else 'connect()'
+                }}
+              />
+            </div>
           </div>
-
+      </div>
     </div>
   )
 }
--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import {ethers } from 'ethers';
+
+import { money } from '../assests'
+import { CustomButton } from '../components'
+import { checkIfImage } from '../utils'
 
 const CreateCampaign = () => {
+  const navigate = useNavigate();
   return (
     <div>CreateCampaign</div>
   )
