8d9997242de3ef4475f5987dee7402ac62afb14c
PriyanshuQ <80645068+PriyanshuQ@users.noreply.github.com>
2023-05-18 22:45:19 +0530

    commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -31,7 +31,36 @@ const Navbar = () => {
             else 'connect()'
           }}
         />
+
+        <Link to="/profile">
+          <div className='w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer'>
+            <img src={thirdweb} alt="user" className='w-[60%] h-[60%] object-contain' />
+          </div>
+        </Link>
       </div>
+
+      {/* Small screen navigation */}
+          <div className='sm:hidden flex justfy-between items-center relative'>
+            <div className='w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer'>
+              <img src={thirdweb} alt="user" className='w-[60%] h-[60%] object-contain' />
+            </div>
+
+          <img 
+            src={menu} 
+            alt="menu" 
+              className='w-[34px] h-[34px] object-contain cursor-pointer'
+              onClick={() => setToggleDrawer(!toggleDrawer)}
+          />
+          <div className={`absolute top-[50px] right-0 left-0 bg-[#1c1c24] z-10 shadow-secondary py-4 ${!toggleDrawer} ? '-translate-y-[100vh]' : 'translate-y-0]' transition-all duration-700`}></div>
+            <ul className='mb-4'>
+              {navlinks.map((link) => (
+                <li>
+                  
+                </li>
+              ))}
+            </ul>
+          </div>
+
     </div>
   )
 }
